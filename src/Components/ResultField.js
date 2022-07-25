d666fecd911a849583a370e813fd89a2987cb1e3
muloneweb <49344807+muloneweb@users.noreply.github.com>
2022-07-25 15:18:48 +0200

    Delete src directory

--- src/Components/ResultField.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default (p) => {
-	return (
-		<>
-			<div className="result_div" style={{ verticalAlign: "bottom", textAlign: "right", fontWeight: "bold", fontSize: "20px" }}>{p.result}</div>
-		</>
-	)
-}
